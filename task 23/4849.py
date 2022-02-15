077c36b48562b5d03bc47d298a3e2de69e9fd3cb
vertunoff <83171047+vertunoff@users.noreply.github.com>
2022-02-15 21:06:38 +0300

    Add files via upload

---
+++ task 23/4849.py
@@ -0,0 +1,15 @@
+def fibonacci(z, n=100):
+    fn = [0, 1]
+    for i in range(2, n):
+        x = fn[i-1] + fn[i-2]
+        if x >= z:
+            return x
+        fn.append(x)
+def f(n, k):
+    if n > k:
+        return 0
+    if n == k:
+        return 1
+    if n < k:
+        return f(n + 1, k) + f(n + 4, k) + f(n + fibonacci(n), k)
+print(f(1, 17)) 
